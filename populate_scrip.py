deb283448ac8383d585c410081066a0926c5200c
Milton <miltondehara@gmail.com>
2015-01-25 13:33:56 -0600

    Modelos Inicializados/index.html agregado

---
+++ populate_scrip.py
@@ -0,0 +1,10 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup()
+
+from rango.models import Category, Page
+
+def populate()
+
---
+++ post/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from post.models import Post,Category
+
+admin.site.register(Post)
+admin.site.register(Category)
+
+
